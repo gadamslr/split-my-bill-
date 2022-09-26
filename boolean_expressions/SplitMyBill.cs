99ef26f996366a0f7d6229fbf6d42c48473fcf25
gadamslr <gadams@longroad.ac.uk>
2022-09-26 12:43:50 +0100

    Initial commit

---
+++ boolean_expressions/SplitMyBill.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolean_expressions
+{
+    internal class Program
+    {
+
+    static void SplitMyBill()
+        {
+            // Declaring variables
+            float bill_total;
+            int people;
+            int tip_percentage;
+            float percentage_decimal;
+            float tip_total;
+            float cost_per_person;
+
+            Console.WriteLine("---Welcome to Split My Bill---");
+
+            Console.WriteLine("What is the total bill?");
+
+            bill_total = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many people are sharing?");
+
+            people = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What percentage tip would you like to leave?");
+
+            tip_percentage = int.Parse(Console.ReadLine());
+
+
+            percentage_decimal = tip_percentage / 100;
+
+            tip_total = bill_total * percentage_decimal;
+
+            bill_total = bill_total + tip_total;
+
+            cost_per_person = bill_total / people;
+
+            Console.WriteLine("Total bill including tip is £{0}", bill_total);
+
+            Console.WriteLine("Total cost per person is £{0}", cost_per_person);
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            SplitMyBill();
+
+            Console.ReadLine();
+        }
+    }
+}
